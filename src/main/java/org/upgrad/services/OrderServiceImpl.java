561b4cd8ca854f5d68936b2749790901bde018e4
chandra.tekam <tekamchandraprakash@gmail.com>
2018-12-02 13:46:03 +0530

    OrderService  : getCoupon method is added and fetched coupon from repository.

--- src/main/java/org/upgrad/services/OrderServiceImpl.java
+++ src/main/java/org/upgrad/services/OrderServiceImpl.java
@@ -1,4 +1,31 @@
 package org.upgrad.services;
 
-public class OrderServiceImpl{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.upgrad.models.Coupon;
+import org.upgrad.models.Order;
+import org.upgrad.repositories.CouponRepository;
+import org.upgrad.requestResponseEntity.ItemQuantity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Override
+    public Coupon getCoupon(String couponName) {
+        return couponRepository.findCouponByName(couponName);
+    }
+
+    @Override
+    public List<Order> getOrdersByUser(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public Integer addOrderWithPermAddress(Integer itemId, Integer quantity, Integer userId, ArrayList<ItemQuantity> itemQuantities, double bill, Integer couponId, double v1) {
+        return null;
+    }
 }
