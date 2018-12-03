1c42ef5e8ae86c5961399eebad2262957475fd33
chandra.tekam <tekamchandraprakash@gmail.com>
2018-12-03 15:42:37 +0530

    PaymentController:  paymentMethod is added for api /payment"

--- src/main/java/org/upgrad/controllers/PaymentController.java
+++ src/main/java/org/upgrad/controllers/PaymentController.java
@@ -1,5 +1,43 @@
 package org.upgrad.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.upgrad.services.PaymentService;
+import org.upgrad.services.UserAuthTokenService;
+
+/**
+ * @author Chandra Prakash Tekam
+ */
+@RestController
+@RequestMapping("/payment")
 public class PaymentController {
+    @Autowired
+    private UserAuthTokenService userAuthTokenService;
+
+    @Autowired
+    private PaymentService paymentService;
+
+    /**
+     * this api fetches the alltypes of payment method by invoking service
+     *
+     * @param accessToken authentication token from header
+     * @return json array of payment method
+     */
+    @GetMapping("")
+    public ResponseEntity<?> paymentMethod(@RequestHeader String accessToken) {
+        if (userAuthTokenService.isUserLoggedIn(accessToken) == null) {
+            return new ResponseEntity<>("Please Login first to access this endpoint!", HttpStatus.UNAUTHORIZED);
+        } else if (userAuthTokenService.isUserLoggedIn(accessToken).getLogoutAt() != null) {
+            return new ResponseEntity<>("You have already logged out. Please Login first to access this endpoint!", HttpStatus.UNAUTHORIZED);
+        } else {
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
 
 }
