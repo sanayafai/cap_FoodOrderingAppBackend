1b955560f271f1fdedd25f557cf9d0bf0028ebe4
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 12:06:13 +0530

    Category model implemented.

--- src/main/java/org/upgrad/models/Category.java
+++ src/main/java/org/upgrad/models/Category.java
@@ -1,5 +1,43 @@
 package org.upgrad.models;
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@Entity
+@Table(name="category")
 public class Category {
+  
+    @Id
+    private int id;
+  
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Transient
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Item item;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 
 }
