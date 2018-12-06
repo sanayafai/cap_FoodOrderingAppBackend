37def0bb94a7f2e266ab2f89ad8280ecba4db2d9
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 16:47:31 +0530

    Item service implemented.

--- src/main/java/org/upgrad/services/ItemService.java
+++ src/main/java/org/upgrad/services/ItemService.java
@@ -1,4 +1,11 @@
 package org.upgrad.services;
 
+import java.util.List;
+
 public interface ItemService {
+  
+    List<Item> getItemByPopularity(int restaurantId);
+
+    Item getItemById(int id);
+  
 }
