77940140523b9b21a50ac134b5d1600068dd819f
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 14:09:35 +0530

    Category Service Implementation implemented.

--- src/main/java/org/upgrad/services/CategoryServiceImpl.java
+++ src/main/java/org/upgrad/services/CategoryServiceImpl.java
@@ -1,4 +1,40 @@
 package org.upgrad.services;
 
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import org.upgrad.models.Category;
+import org.upgrad.repositories.CategoryRepository;
+
+@Service
+@Transactional
 public class CategoryServiceImpl{
+  
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Iterable<Category> getAllCategories() {
+        return categoryRepository.getCategories();
+    }
+
+    @Override
+    public Category getCategory(String categoryName){
+
+        String s1 = categoryName.substring(0, 1).toUpperCase();
+        String nameCapitalized = s1 + categoryName.substring(1).toLowerCase();
+        return categoryRepository.getCategoryByName(nameCapitalized);
+}
+
+    @Override
+    public Integer getCategoryCount(String categoryName){
+
+        String s1 = categoryName.substring(0, 1).toUpperCase();
+        String nameCapitalized = s1 + categoryName.substring(1).toLowerCase();
+        return categoryRepository.getCategoryCountByName(nameCapitalized);
+    }
+  
 }
