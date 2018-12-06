4cad9fab7e2473b5dcb399d62fe640ea4e9eb6fb
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 14:09:21 +0530

    Category Service implemented.

--- src/main/java/org/upgrad/services/CategoryService.java
+++ src/main/java/org/upgrad/services/CategoryService.java
@@ -1,5 +1,13 @@
 package org.upgrad.services;
 
+import org.upgrad.models.Category;
+
 public interface CategoryService {
+  
+    Iterable<Category> getAllCategories();
+
+    Category getCategory(String categoryName);
+
+    Integer getCategoryCount(String categoryName);
 
 }
