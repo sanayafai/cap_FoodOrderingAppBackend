38dd3aee5e7381d63caddf060d8ac85ee8f4ea15
chandra.tekam <tekamchandraprakash@gmail.com>
2018-12-06 12:45:39 +0530

    adddress respository object is crated

--- src/main/java/org/upgrad/services/OrderServiceImpl.java
+++ src/main/java/org/upgrad/services/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.upgrad.models.Coupon;
 
 import org.upgrad.models.Order;
+import org.upgrad.repositories.AddressRepository;
 import org.upgrad.repositories.CouponRepository;
 import org.upgrad.repositories.OrderRepository;
 import org.upgrad.requestResponseEntity.ItemQuantity;
@@ -20,10 +21,15 @@ public class OrderServiceImpl implements OrderService {
 
     private OrderRepository orderRepository;
 
-    public OrderServiceImpl(CouponRepository couponRepository, OrderRepository orderRepository) {
+    private final AddressRepository addressRepository;
+
+    public OrderServiceImpl(CouponRepository couponRepository, OrderRepository orderRepository, AddressRepository addressRepository) {
+
         this.couponRepository = couponRepository;
 
         this.orderRepository = orderRepository;
+
+        this.addressRepository =  addressRepository;
     }
 
     @Override
@@ -43,6 +49,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Integer addOrder(String flatBuilNo, String locality, String city, String zipcode, int stateId, String type, int paymentId, Integer userId, List<ItemQuantity> itemQuantities, double bill, Integer couponId, double discount) {
+
         return null;
     }
 
