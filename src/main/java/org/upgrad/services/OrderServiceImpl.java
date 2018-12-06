d318f09819e43300671c35fb68a840c9bb4cd0b0
chandra.tekam <tekamchandraprakash@gmail.com>
2018-12-06 11:33:47 +0530

    add order second method aadded

--- src/main/java/org/upgrad/services/OrderServiceImpl.java
+++ src/main/java/org/upgrad/services/OrderServiceImpl.java
@@ -2,14 +2,14 @@ package org.upgrad.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.upgrad.models.Coupon;
+import org.upgrad.models.*;
 
-import org.upgrad.models.Order;
 import org.upgrad.repositories.CouponRepository;
 import org.upgrad.repositories.OrderRepository;
 import org.upgrad.requestResponseEntity.ItemQuantity;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -38,12 +38,35 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Integer addOrderWithPermAddress(Integer addressId, Integer paymentId, Integer userId, ArrayList<ItemQuantity> itemQuantities, double bill, Integer couponId, double discount) {
-        return null;
+        return orderRepository.saveOrder(bill, couponId, discount, paymentId, userId, addressId);
     }
 
     @Override
-    public Integer addOrder(String flatBuilNo, String locality, String city, String zipcode, int stateId, String type, int paymentId, Integer userId, List<ItemQuantity> itemQuantities, double bill, Integer couponId, double discount) {
-        return null;
+    public Integer addOrder(String flatBuilNo, String locality, String city, String zipcode,
+                            int stateId, String type, int paymentId, Integer userId,
+                            List<ItemQuantity> itemQuantities, double bill, Integer couponId, double discount) {
+        Order order = new Order();
+        order.setBill(bill);
+        order.setDiscount(discount);
+        Address address = new Address();
+        address.setCity(city);
+        address.setFlatBuilNumber(flatBuilNo);
+        address.setLocality(locality);
+        States states = new States();
+        states.setId(stateId);
+        address.setState(states);
+        order.setAddress(address);
+        Payment payment = new Payment();
+        payment.setId(paymentId);
+        order.setPayment(payment);
+        User user = new User();
+        user.setId(userId);
+        order.setUser(user);
+        order.setDate(new Date());
+        //   order.setOrderItems(itemQuantities);
+
+        Order persistOrder = orderRepository.save(order);
+        return persistOrder.getId();
     }
 
 
