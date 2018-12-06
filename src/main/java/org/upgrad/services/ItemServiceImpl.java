745066df813652a56f1da7649b599fc4bf92499b
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 16:47:38 +0530

    Item service implementation implemented.

--- src/main/java/org/upgrad/services/ItemServiceImpl.java
+++ src/main/java/org/upgrad/services/ItemServiceImpl.java
@@ -1,4 +1,34 @@
 package org.upgrad.services;
 
-public class ItemServiceImpl{
+import java.util.ArrayList;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.upgrad.models.Item;
+import org.upgrad.repositories.ItemRepository;
+
+public class ItemServiceImpl implements ItemService{
+  
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Override
+    public Item getItemById(int id){
+        return itemRepository.getItemById(id);
+    }
+  
+    @Override
+    public List<Item> getItemByPopularity(int restaurantId) {
+        Iterable<Integer> itemIds = itemRepository.getItems(restaurantId);
+        List<Item> itemList = new ArrayList();
+
+        for (Integer id: itemIds) {
+            Item item = itemRepository.getItemById(id);
+            itemList.add(item);
+        }
+        return itemList;
+    }
+  
 }
