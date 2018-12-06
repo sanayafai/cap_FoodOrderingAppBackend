3301711a05235156a6a54321554e48e2212a9005
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 18:16:32 +0530

    Bug fixed.

--- src/main/java/org/upgrad/repositories/ItemRepository.java
+++ src/main/java/org/upgrad/repositories/ItemRepository.java
@@ -1,6 +1,13 @@
 package org.upgrad.repositories;
 
-public interface ItemRepository{
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Set;
+import org.upgrad.models.Item;
+
+@Repository
+public interface ItemRepository extends CrudRepository<Item,Integer> {
   
     @Query(nativeQuery = true,value="SELECT * from ITEM WHERE ID=?1")
     Item getItemById(int id);
