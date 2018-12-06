43cd20a1c2586efc1e1fe4e7bb6015a027b842e1
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 18:16:28 +0530

    Bug fixed.

--- src/main/java/org/upgrad/controllers/ItemController.java
+++ src/main/java/org/upgrad/controllers/ItemController.java
@@ -1,5 +1,6 @@
 package org.upgrad.controllers;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.upgrad.models.Item;
+import org.upgrad.services.ItemService;
+
+@RestController
+@RequestMapping("/item/restaurant")
 public class ItemController {
   
   @Autowired
