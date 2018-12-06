d528bb2cf30e516f16830d42e7559366cf909089
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 18:16:41 +0530

    Imported id class.

--- src/main/java/org/upgrad/services/ItemService.java
+++ src/main/java/org/upgrad/services/ItemService.java
@@ -1,11 +1,11 @@
 package org.upgrad.services;
 
+import org.upgrad.models.Item;
 import java.util.List;
 
 public interface ItemService {
   
     List<Item> getItemByPopularity(int restaurantId);
-
     Item getItemById(int id);
   
 }
