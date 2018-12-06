d45ee5efb3ef9851b6bcbab0107b972bd70387c6
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-06 19:44:23 +0530

    Item model imported and bug fixed.

--- src/main/java/org/upgrad/repositories/CategoryRepository.java
+++ src/main/java/org/upgrad/repositories/CategoryRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import org.upgrad.models.Item;
 import org.upgrad.models.Category;
 
 @Repository
-public interface CategoryRepository {
+public interface CategoryRepository extends CrudRepository<Category,Integer> {
   
     @Query(nativeQuery = true,value = "SELECT * FROM CATEGORY")
     Iterable<Category> getCategories();
